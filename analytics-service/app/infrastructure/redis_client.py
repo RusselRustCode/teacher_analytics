a4ec4aa2ace54dc4dc01281fcbf313f7c52f7d3e
RusselDRust <noobboy0307@gmail.com>
2025-12-27 01:06:39 +0500

    changed prefix for redis

--- analytics-service/app/infrastructure/redis_client.py
+++ analytics-service/app/infrastructure/redis_client.py
@@ -7,7 +7,7 @@ class RedisCache:
 
     async def set_analytics(self, student_id: int, data: dict):
         await self.client.set(f"analytics:{student_id}", json.dumps(data), ex=3600)
-
+        
     async def get_analytics(self, student_id: int):
         data = await self.client.get(f"analytics:{student_id}")
         return json.loads(data) if data else None
