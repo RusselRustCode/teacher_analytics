f95f53300507eb5dd9331819037645f8dd807b7c
RusselDRust <noobboy0307@gmail.com>
2025-12-27 01:05:51 +0500

    add logging

--- analytics-service/app/infrastructure/kafka_consumer.py
+++ analytics-service/app/infrastructure/kafka_consumer.py
@@ -6,16 +6,25 @@ class AnalyticsConsumer:
         self.consumer = AIOKafkaConsumer(
             topic,
             bootstrap_servers=brokers,
-            group_id="analytics_group"
+            group_id="analytics_group_v2", 
+            auto_offset_reset="earliest",  # Читать всё с начала, если группа новая
+            value_deserializer=lambda m: json.loads(m.decode('utf-8'))
         )
         self.service = service 
 
     async def start(self):
-        await self.consumer.start()
+        print(f"--- Попытка подключения к Kafka... ---")
         try:
+            await self.consumer.start()
+            print(f"--- Успешное подключение! Слушаю топик... ---")
+            
             async for msg in self.consumer:
-                data = json.loads(msg.value)
-                print(f"Поступил лог на студента {data['student_id']}, triggering update...")
+                data = msg.value 
+                s_id = data.get('student_id') or data.get('log_data', {}).get('student_id')
+                print(f"--- [KAFKA] Получены данные для студента {s_id} ---")
+                
                 await self.service.process_new_log(data) 
+        except Exception as e:
+            print(f"--- [KAFKA ERROR] Ошибка: {e} ---")
         finally:
             await self.consumer.stop()
