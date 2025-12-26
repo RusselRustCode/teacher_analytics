62df82c265518291ada4bacb7918102b6a34481b
RusselDRust <noobboy0307@gmail.com>
2025-12-27 02:01:15 +0500

    changed func LoadConfig and did it more flexible

--- core-service/internal/config/config.go
+++ core-service/internal/config/config.go
@@ -12,14 +12,22 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	return &Config{
-		HTTPPort:      getEnv("HTTP_PORT", "8080"),
-		GRPCPort:      getEnv("GRPC_PORT", "50051"),
-		DBDSN:         "postgres://admin:admin@postgres:5432/student_analytics?sslmode=disable",
-		RedisAddr:     getEnv("REDIS_HOST", "redis") + ":6379",
-		KafkaAddr:     getEnv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9094"),
-		AnalyticsAddr: getEnv("ANALYTICS_GRPC_HOST", "analytics-service") + ":50052",
-	}
+    user := getEnv("DB_USER", "admin")
+    pass := getEnv("DB_PASSWORD", "admin_password")
+    host := getEnv("DB_HOST", "student-analytics-postgres") 
+    port := getEnv("DB_PORT", "5432")
+    name := getEnv("DB_NAME", "student_analytics")
+
+    dsn := "postgres://" + user + ":" + pass + "@" + host + ":" + port + "/" + name + "?sslmode=disable"
+
+    return &Config{
+        HTTPPort:      getEnv("HTTP_PORT", "8080"),
+        GRPCPort:      getEnv("GRPC_PORT", "50051"),
+        DBDSN:         dsn,
+        RedisAddr:     getEnv("REDIS_HOST", "redis") + ":" + getEnv("REDIS_PORT", "6379"),
+        KafkaAddr:     getEnv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9094"),
+        AnalyticsAddr: getEnv("ANALYTICS_GRPC_HOST", "analytics-service") + ":" + getEnv("ANALYTICS_GRPC_PORT", "50052"),
+    }
 }
 
 func getEnv(key, fallback string) string {
