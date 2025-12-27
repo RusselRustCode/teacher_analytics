a18c43ce7d42f91ea33615fff3f60025cd81251f
RusselDRust <noobboy0307@gmail.com>
2025-12-27 07:50:04 +0500

    Work in progress: saving local changes before pull

--- core-service/internal/infrastructure/kafka/interface.go
+++ core-service/internal/infrastructure/kafka/interface.go
@@ -3,7 +3,7 @@ package kafka
 import (
 	"github.com/RusselRustCode/teacher_analytics/core-service/internal/domain"
 )
-
+//Простая заглушка
 type MessageProducer interface {
 	SendLog(log *domain.StudentLog) error
 	Close() error
